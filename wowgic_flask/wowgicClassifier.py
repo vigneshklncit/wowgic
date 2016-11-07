89c3d518fc2afb1b0241e047125edc30a0ce2811
chelladurai pandian <chelladuraipandian@chelladurais-MacBook-Air.local>
2016-11-07 12:14:06 +0530

    Started implementing wowgicclassififer

--- wowgic_flask/wowgicClassifier.py
+++ wowgic_flask/wowgicClassifier.py
@@ -11,200 +11,104 @@ from statistics import mode
 from nltk.tokenize import word_tokenize
 import re
 
-
-class VoteClassifier(ClassifierI):
-    def __init__(self, *classifiers):
-        self._classifiers = classifiers
-
-    def classify(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-        return mode(votes)
-
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-
-        choice_votes = votes.count(mode(votes))
-        conf = choice_votes / len(votes)
-        return conf
-
-def extract_entity_names(t):
-    entity_names = []
-    
-    if hasattr(t, 'node') and t.node:
-        if t.node == 'NE':
-            entity_names.append(' '.join([child[0] for child in t]))
-        else:
-            for child in t:
-                entity_names.extend(extract_entity_names(child))
-                
-    return entity_names
+class wowgicNaiveBayes:
+    def __init__(self, feeds):
+        self.feeds = feeds
+        self.all_words = []
+        self.documents = {}
 
 
-short_news = open("news.txt","r").read()
-short_spams = open("spams.txt","r").read()
-short_conversations = open("conversations.txt","r").read()
-short_porn = open("porn.txt","r").read()
+    def find_features(document):
+        words = word_tokenize(document)
+        features = {}
+        for w in word_features:
+            features[w] = (w in words)
+        print('inside find features',features[w])
+        return features
 
-# move this up here
-all_words = []
-documents = []
-i=0
+    def createClassifiers(self):
+        for tweet in self.feeds:
+            if 'text' in tweet:
+                sent = tweet['text']
+                sent=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",sent).split())
+                sent = sent.replace("RT", "", 1)
+                category = tweet['category']
+                self.documents.append(sent, category)
+                words = word_tokenize(sent)
+                for w in words:
+                    all_words.append(w)
 
-#  j is adject, r is adverb, and v is verb
-#allowed_word_types = ["J","R","V"]
-allowed_word_types = []
+        save_documents = open("pickled_algos/documents.pickle","wb")
+        pickle.dump(documents, save_documents)
+        save_documents.close()
+        all_words = nltk.FreqDist(all_words)
+        word_features = list(all_words.keys())[:5000]
+        save_word_features = open("pickled_algos/word_features5k.pickle","wb")
+        pickle.dump(word_features, save_word_features)
+        save_word_features.close()
 
-for p in short_news.split('\n'):
-    p=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",p).split())
-    p =p.replace("RT", "", 1)
-    documents.append( (p, "news") )
-      
-    words = word_tokenize(p)
+        featuresets = [(find_features(rev), category) for (rev, category) in documents]
         
-    pos = nltk.pos_tag(words)
-    for w in pos:
-        if w[1][0] not in allowed_word_types:
-            all_words.append(w[0].lower())
+        random.shuffle(featuresets)
+        save_feature_sets = open("pickled_algos/featuresets.pickle","wb")
+        pickle.dump(featuresets, save_feature_sets)
+        save_feature_sets.close()
+        print(len(featuresets))
+        testing_set = featuresets[17:]
+        training_set = featuresets[:17]
 
-   
-for p in short_spams.split('\n'):
-    p=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",p).split())
-    p =p.replace("RT", "", 1)
-    documents.append( (p, "spam") )
-    words = word_tokenize(p)
-    pos = nltk.pos_tag(words)
-    for w in pos:
-        i=i+1
-        if w[1][0] not in allowed_word_types:
-            all_words.append(w[0].lower())
+        classifier = nltk.NaiveBayesClassifier.train(training_set)
+        print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+        classifier.show_most_informative_features(15)
 
-for p in short_conversations.split('\n'):
-    p=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",p).split())
-    p =p.replace("RT", "", 1)
-    documents.append( (p, "conversations") )
-    words = word_tokenize(p)
-    i=i+1
-    pos = nltk.pos_tag(words)
-    for w in pos:
-        if w[1][0] not in allowed_word_types:
-            all_words.append(w[0].lower())
+        ###############
+        save_classifier = open("pickled_algos/originalnaivebayes5k.pickle","wb")
+        pickle.dump(classifier, save_classifier)
+        save_classifier.close()
 
-for p in short_porn.split('\n'):
-    p=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",p).split())
-    p =p.replace("RT", "", 1)
-    documents.append( (p, "porn") )
-    words = word_tokenize(p)
-    i=i+1
-    pos = nltk.pos_tag(words)
-    entity_names = []
-    namedEnt = nltk.ne_chunk(pos, binary=True)
-    print namedEnt
-    for tree in namedEnt:
-        # Print results per sentence
-        # print extract_entity_names(tree)
-        entity_names.extend(extract_entity_names(tree))
-    print set(entity_names)
-    for w in pos:
-        print(i,"****",w)
-        if w[1][0] not in allowed_word_types:
-            all_words.append(w[0].lower())
+        MNB_classifier = SklearnClassifier(MultinomialNB())
+        MNB_classifier.train(training_set)
+        print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
 
-save_documents = open("pickled_algos/documents.pickle","wb")
-pickle.dump(documents, save_documents)
-save_documents.close()
+        save_classifier = open("pickled_algos/MNB_classifier5k.pickle","wb")
+        pickle.dump(MNB_classifier, save_classifier)
+        save_classifier.close()
+
+        BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+        BernoulliNB_classifier.train(training_set)
+        print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+        save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
+        pickle.dump(BernoulliNB_classifier, save_classifier)
+        save_classifier.close()
+
+        LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+        LogisticRegression_classifier.train(training_set)
+        print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+        save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
+        pickle.dump(LogisticRegression_classifier, save_classifier)
+        save_classifier.close()
 
 
-all_words = nltk.FreqDist(all_words)
+        LinearSVC_classifier = SklearnClassifier(LinearSVC())
+        LinearSVC_classifier.train(training_set)
+        print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+        save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
+        pickle.dump(LinearSVC_classifier, save_classifier)
+        save_classifier.close()
 
 
-word_features = list(all_words.keys())[:5000]
+        ##NuSVC_classifier = SklearnClassifier(NuSVC())
+        ##NuSVC_classifier.train(training_set)
+        ##print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
 
 
-save_word_features = open("pickled_algos/word_features5k.pickle","wb")
-pickle.dump(word_features, save_word_features)
-save_word_features.close()
+        SGDC_classifier = SklearnClassifier(SGDClassifier())
+        SGDC_classifier.train(training_set)
+        print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
 
-
-def find_features(document):
-    words = word_tokenize(document)
-    features = {}
-    for w in word_features:
-        features[w] = (w in words)
-    print('inside find features',features[w])
-    return features
-
-featuresets = [(find_features(rev), category) for (rev, category) in documents]
-
-random.shuffle(featuresets)
-
-save_feature_sets = open("pickled_algos/featuresets.pickle","wb")
-pickle.dump(featuresets, save_feature_sets)
-save_feature_sets.close()
-
-print(len(featuresets))
-
-testing_set = featuresets[10:]
-training_set = featuresets[:10]
-
-
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
-classifier.show_most_informative_features(15)
-
-###############
-save_classifier = open("pickled_algos/originalnaivebayes5k.pickle","wb")
-pickle.dump(classifier, save_classifier)
-save_classifier.close()
-
-MNB_classifier = SklearnClassifier(MultinomialNB())
-MNB_classifier.train(training_set)
-print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/MNB_classifier5k.pickle","wb")
-pickle.dump(MNB_classifier, save_classifier)
-save_classifier.close()
-
-BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
-BernoulliNB_classifier.train(training_set)
-print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
-pickle.dump(BernoulliNB_classifier, save_classifier)
-save_classifier.close()
-
-LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-LogisticRegression_classifier.train(training_set)
-print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
-pickle.dump(LogisticRegression_classifier, save_classifier)
-save_classifier.close()
-
-
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(training_set)
-print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
-pickle.dump(LinearSVC_classifier, save_classifier)
-save_classifier.close()
-
-
-##NuSVC_classifier = SklearnClassifier(NuSVC())
-##NuSVC_classifier.train(training_set)
-##print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
-
-
-SGDC_classifier = SklearnClassifier(SGDClassifier())
-SGDC_classifier.train(training_set)
-print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
-
-save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
-pickle.dump(SGDC_classifier, save_classifier)
-save_classifier.close()
+        save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
+        pickle.dump(SGDC_classifier, save_classifier)
+        save_classifier.close()
