57a4b8f1ae19dcbbaf80a17842777959479538b2
chelladurai pandian <chelladuraipandian@chelladurais-MacBook-Air.local>
2016-11-08 09:23:13 +0530

    Implemented naivebayes classifier standalone

---
+++ wowgic_flask/sentiment_final.py
@@ -0,0 +1,5 @@
+import sentiment_mod as s
+
+print(s.sentiment("#s33millionviews is now trending in #chennai"))
+print(s.sentiment("#xxxdeepikatrailer is now trending in #Chennai https://t.co/E9nWJ0VKV5"))
+print(s.sentiment("Just 500 mts of something needs to be completed and chennai will have an integrated train system from Mount Railway station"))
